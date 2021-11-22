ffc98ad44f62864458a4727a253d2eb8fec7226a
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-22 13:39:56 +1100

    React Advance Tutorial - Add useState hook to update the title in a React component

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+//useState - function
+// is used to update the state in a React component
+// must be in the function/component body
+// cannot call conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random Title");
+
+  const handleClick = () => {
+    if (text === "Random Title") {
+      setText("My value changed");
+    } else {
+      setText("Random Title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
