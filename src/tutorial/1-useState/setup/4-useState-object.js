f84908406c721f2614a361efdd6456f57c7e3384
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-12-01 13:07:37 +1100

    React Advance Tutorial - Add useState object and update value

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [people, setPeople] = useState({
+    name: "Peter",
+    age: 24,
+    message: "Random Message",
+  });
+
+  const changeMessage = () => {
+    setPeople({ ...people, message: "Message Changed" });
+  };
+  return (
+    <>
+      <h2>{people.name}</h2>
+      <h3>{people.age}</h3>
+      <h4>{people.message}</h4>
+      <button onClick={changeMessage} className="btn">
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
