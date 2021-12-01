d03559c4dd98308ab6c90b8ed1dff1dea7c86f5a
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-12-01 15:43:18 +1100

    React Advance Tutorial - Create complex counter with using setTimeout method to increase the value of the counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,6 +5,15 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1); Using normal setValue won't behave as expected with setTimeout method
+      // Need to use functional setValue to keep the previous value if button clicked couple of times
+      setValue((preValue) => {
+        return preValue + 1;
+      });
+    }, 2000);
+  };
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
@@ -17,6 +26,13 @@ const UseStateCounter = () => {
           Reset
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Complext Counter</h2>
+        <h3>{value}</h3>
+        <button className="btn" onClick={complexIncrease}>
           Increase
         </button>
       </section>
