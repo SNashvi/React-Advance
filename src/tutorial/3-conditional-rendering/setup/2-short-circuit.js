19ae6094d074255487cd62d9183588c0cfd6257f
Shohreh Nashvi <shohreh@i-motor.com.au>
2022-01-10 13:35:26 +1100

    React Advance Tutorial - Add Short Circuit Evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text || "This line only appears when text is empty."}</h1>
+      {text && <h1>This line only appears when text is not empty</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
