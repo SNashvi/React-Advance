f3b3a025ecb4efd68ba37e24731c0b0d782a278a
Shohreh Nashvi <shohreh@i-motor.com.au>
2022-01-12 16:30:21 +1100

    React Advance Tutorial - Use button to toggle

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,14 +3,17 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+  // const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
   return (
     <>
-      <h1>{text || "This line only appears when text is empty."}</h1>
-      {text && <h1>This line only appears when text is not empty</h1>}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h2>Error...</h2>}
     </>
   );
 };
