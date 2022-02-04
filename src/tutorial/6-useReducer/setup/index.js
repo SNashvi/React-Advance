dd29e6f9f0637fb2b440cfd949870af0a1fd67a3
Shohreh Nashvi <shohreh@i-motor.com.au>
2022-02-04 11:19:53 +1100

    React Advance Tutorial - Create a form to add/remove items using useReducer Hook

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,57 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
+import { reducer } from "./reducer";
 
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  //When invoking useReducer we get State Value and Dispatch Function
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newPeople = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newPeople });
+      setName("");
+    } else {
+      dispatch({ type: "NO_VALUE" });
+    }
+  };
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+  return (
+    <>
+      {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          ></input>
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id} className="item">
+            <h4>{person.name}</h4>
+            <button onClick={() => dispatch({ type: "REMOVE_ITEM", payload: person.id })}>Remove</button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
