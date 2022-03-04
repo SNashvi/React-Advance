72c0d5cc6f1a33047738f2ae7a1b6200a71aef13
Shohreh Nashvi <shohreh@i-motor.com.au>
2022-03-04 16:44:59 +1100

    React Advance Tutorial - Publish it to GitHub Pages

---
+++ build/precache-manifest.700517f15b6d8620a4d1196716732619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f62e37043ac80eb13205cd3d5a0f53",
+    "url": "/React-Advance/index.html"
+  },
+  {
+    "revision": "12a1806ab845cd7243f5",
+    "url": "/React-Advance/static/css/main.c6cc0205.chunk.css"
+  },
+  {
+    "revision": "022945d9b2c0c50813ea",
+    "url": "/React-Advance/static/js/2.2a5a8f58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Advance/static/js/2.2a5a8f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a1806ab845cd7243f5",
+    "url": "/React-Advance/static/js/main.221f678f.chunk.js"
+  },
+  {
+    "revision": "648f27e8f048befb07fa",
+    "url": "/React-Advance/static/js/runtime-main.0a742fa8.js"
+  }
+]);
